cb8cd611a53640a95d0338e8c337054a9eb994e9
Peter Harrison <peter.harrison@helicron.net>
2022-12-30 12:54:57 +0000

    add slip coefficient to parameters

--- parameters.py
+++ parameters.py
@@ -13,18 +13,19 @@ class TurnParameters:
     angle: float = 0.0
     length: float = 0.0
     gamma: float = 0.0
+    slip_coefficient: float = 0.0
 
 
 default_params = {
-    "SS90F": TurnParameters(pivot_x=0, pivot_y=0, offset=150, delta=68, radius=112, startAngle=0, angle=90, speed=2200, length=248),
-    "SS180": TurnParameters(pivot_x=0, pivot_y=0, offset=160, delta=127, radius=85, startAngle=0, angle=180, speed=2000, length=370),
-    "SD45": TurnParameters(pivot_x=0, pivot_y=0, offset=160, delta=65, radius=110, startAngle=0, angle=45, speed=2200, length=134),
-    "SD135": TurnParameters(pivot_x=0, pivot_y=0, offset=150, delta=90, radius=83, startAngle=0, angle=135, speed=1800, length=266),
-    "DS45": TurnParameters(pivot_x=90, pivot_y=0, offset=75, delta=70, radius=120, startAngle=45, angle=45, speed=2200, length=144),
-    "DS135": TurnParameters(pivot_x=90, pivot_y=0, offset=105, delta=90, radius=80, startAngle=45, angle=135, speed=1800, length=257),
-    "DD90": TurnParameters(pivot_x=90, pivot_y=0, offset=105, delta=80, radius=74, startAngle=45, angle=90, speed=1800, length=173),
-    "SS90E": TurnParameters(pivot_x=0, pivot_y=0, offset=75, delta=50, radius=56, startAngle=0, angle=90, speed=1000, length=124),
-    "DD90K": TurnParameters(pivot_x=0, pivot_y=0, offset=100, delta=50, radius=199, startAngle=45, angle=90, speed=1000, length=371),
+    "SS90F": TurnParameters(pivot_x=0, pivot_y=0, offset=150, delta=68, radius=112, startAngle=0, angle=90, speed=2200, length=248,slip_coefficient = 0.0),
+    "SS180": TurnParameters(pivot_x=0, pivot_y=0, offset=160, delta=127, radius=85, startAngle=0, angle=180, speed=2000, length=370,slip_coefficient = 0.0),
+    "SD45": TurnParameters(pivot_x=0, pivot_y=0, offset=160, delta=65, radius=110, startAngle=0, angle=45, speed=2200, length=134,slip_coefficient = 0.0),
+    "SD135": TurnParameters(pivot_x=0, pivot_y=0, offset=150, delta=90, radius=83, startAngle=0, angle=135, speed=1800, length=266,slip_coefficient = 0.0),
+    "DS45": TurnParameters(pivot_x=90, pivot_y=0, offset=75, delta=70, radius=120, startAngle=45, angle=45, speed=2200, length=144,slip_coefficient = 0.0),
+    "DS135": TurnParameters(pivot_x=90, pivot_y=0, offset=105, delta=90, radius=80, startAngle=45, angle=135, speed=1800, length=257,slip_coefficient = 0.0),
+    "DD90": TurnParameters(pivot_x=90, pivot_y=0, offset=105, delta=80, radius=74, startAngle=45, angle=90, speed=1800, length=173,slip_coefficient = 0.0),
+    "SS90E": TurnParameters(pivot_x=0, pivot_y=0, offset=75, delta=50, radius=56, startAngle=0, angle=90, speed=1000, length=124,slip_coefficient = 0.0),
+    "DD90K": TurnParameters(pivot_x=0, pivot_y=0, offset=100, delta=50, radius=199, startAngle=45, angle=90, speed=1000, length=371,slip_coefficient = 0.0),
 }
 
 working_params = {
