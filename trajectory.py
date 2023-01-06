392c46323d5c111296bad7096058693823c074ea
Peter Harrison <peter.harrison@helicron.net>
2023-01-06 12:03:59 +0000

    fixup trajectory

--- trajectory.py
+++ trajectory.py
@@ -6,7 +6,7 @@
 # File Created: Thursday, 5th January 2023 2:10:17 pm
 # Author: Peter Harrison 
 # -----
-# Last Modified: Friday, 6th January 2023 9:17:24 am
+# Last Modified: Friday, 6th January 2023 12:03:58 pm
 # -----
 # Copyright 2022 - 2023 Peter Harrison, Micromouseonline
 # -----
@@ -40,9 +40,9 @@ import numpy as np
 
 
 class Profile:
-    def __init__(self):
+    def __init__(self, params = None):
         self.length = 0
-        self.params = None
+        self.params = params
         pass
 
     def setup(self, params : TurnParameters):
@@ -54,7 +54,7 @@ class Profile:
             return 0.0
         x = max(0.0,p)
         x = min(1.0,p)
-        x = p * self.params.cubic_length
+        x = p * self.params.length
         return x
 
 class Cubic(Profile):
@@ -63,7 +63,7 @@ class Cubic(Profile):
 
     def setup(self, params : TurnParameters):
         self.params = params
-        self.k = 6.0*self.params.angle/ self.params.cubic_length
+        self.k = 6.0*self.params.angle/ self.params.length
 
     def get_omega(self,p,speed):
         omega = speed * self.k * p * (1-p)
@@ -96,6 +96,7 @@ class Trapezoid(Profile):
             distance = distance - self.params.delta - self.arc_length
             omega = self.arc_omega * (1.0 - distance/self.params.delta)
             phase = 3
+        (omega)
         return (omega,phase)
 
 class Quadratic(Profile):
@@ -104,7 +105,7 @@ class Quadratic(Profile):
 
     def setup(self, params : TurnParameters):
         self.params = params
-        self.arc_omega = np.degrees(params.speed / params.arc_radius)
+        self.arc_omega = np.degrees(params.speed / params.radius)
         self.transition_angle = params.delta * self.arc_omega / (3.0 * params.speed)
         self.arc_angle = params.angle - 2 * self.transition_angle
         self.arc_length = params.speed * self.arc_angle / self.arc_omega
@@ -131,7 +132,7 @@ class Sinusoid(Profile):
 
     def setup(self, params : TurnParameters):
         self.params = params
-        self.arc_omega = np.degrees(params.speed / params.arc_radius)
+        self.arc_omega = np.degrees(params.speed / params.radius)
         self.transition_angle = params.delta * self.arc_omega / (np.pi * params.speed)
         self.arc_angle = params.angle - 2 * self.transition_angle
         self.arc_length = params.speed * self.arc_angle / self.arc_omega
@@ -201,17 +202,20 @@ class Trajectory:
         #
         self.profiler = None
 
-    def set_params(self, start_x, start_y, start_angle, turn_angle, speed):
-        self.turn_angle = np.radians(turn_angle) # radians
+    def set_params(self, start_x, start_y, start_angle, speed):
+        # self.turn_angle = np.radians(turn_angle) # radians
         self.start_x = start_x # mm
         self.start_y = start_y # mm
         self.start_angle = np.radians(start_angle) # radians
         self.speed = speed # mm/s
-        # self.reset_data()
+        self.reset_data()
         #        
 
     def reset_data(self):
-        self.n_items = len(self.time)
+        if self.speed < 1:
+            return
+        self.end_time = self.profiler.length/self.speed
+        self.n_items = int(self.end_time/self.delta_t)
         self.theta_ideal = np.zeros(self.n_items)
         self.omega_ideal = np.zeros(self.n_items)
         self.x_ideal = np.zeros(self.n_items)
@@ -227,12 +231,13 @@ class Trajectory:
         
     def set_speed(self, speed):
         self.speed = speed
-        self.end_time = self.profiler.length/self.speed
-        self.n_items = int(self.end_time/self.delta_t)
 
-        
+    def set_start_xy(self,x,y):
+        self.start_x = x
+        self.start_y = y
 
-
+    def set_start_angle(self,angle):
+        self.start_angle = angle
 
     def calculate(self):
         # if self.n_items == 0:
@@ -241,15 +246,17 @@ class Trajectory:
         if self.profiler is None:
             print("no profiler")
             return
+        self.reset_data()        
+        print(f"{self.end_time:.3f} {self.n_items}")
         self.time = np.linspace(0,self.end_time,self.n_items)
-        self.reset_data()
-        print("calculating")
+        print("calculating...")
         for i,t in enumerate(self.time):
             p = t/self.end_time
             omega,phase = self.profiler.get_omega(p,self.speed)
-            self.omega_actual[i] = omega
-            print(i,omega)
             print(f"{t:.4f}  {omega:.4f}")
+
+    
+
 
 
 
@@ -260,7 +267,11 @@ if __name__ == "__main__":
     profile = Trapezoid()
     trajectory.set_profiler(profile)
     profile.setup(default_params["SS90F"])
-    trajectory.set_params(0,0,0,90,1500)    
+
+    trajectory.set_start_xy(0,0)   
+    trajectory.set_start_angle(0)   
+    trajectory.set_speed(1000)
+
     trajectory.calculate()
-    print(trajectory.omega_actual)
+    print(default_params["SS90F"])
 
